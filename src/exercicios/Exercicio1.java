6605756de4a72b6d045639618ebbd0682cba181d
Rodolfo R. Martins <rodolformartins@outlook.com>
2021-08-31 20:29:36 -0300

    first commit

---
+++ src/exercicios/Exercicio1.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+import java.util.Scanner;
+
+
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		
+			//Exercício 1020
+		
+	       Scanner scan = new Scanner(System.in);
+			
+			//Informando a idade em dias
+		    int idadeEmDias = scan.nextInt();
+		    
+		    int ano = idadeEmDias / 365;
+		    int resto = idadeEmDias % 365;
+		    int meses = resto / 30;
+		    int dias = resto - meses * 30;
+		    
+		    
+		    System.out.println(ano + " ano (s)");
+		    System.out.println(meses + " mes (es)");
+		    System.out.println(dias + " dia (s)");
+	}
+}
